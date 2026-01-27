eee18e94199dbb7651addd74c2928b4c6615a3c1
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-27 13:40:47 +0100

    Create History.js

---
+++ src/History.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function History() {
+  return (
+    <div className="card">
+      <h2>Transaction History</h2>
+      <p>No transactions yet.</p>
+    </div>
+  );
+}
