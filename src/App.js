e5cc18bd449e78761463fc37ff17848ef16022b9
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-27 16:23:30 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./Navbar";
-import Dashboard from "./Dashboard";
-import Plans from "./Plans";
-import Deposit from "./Deposit";
-import Withdraw from "./Withdraw";
-import History from "./History";
-import WalletBox from "./WalletBox";
-import Referrals from "./Referrals";
 import Login from "./Login";
 import Register from "./Register";
+import Dashboard from "./Dashboard";
+import Deposit from "./Deposit";
+import Withdraw from "./Withdraw";
+import Plans from "./Plans";
+import Referrals from "./Referrals";
 import Admin from "./Admin";
 import AdminPayments from "./AdminPayments";
 
@@ -18,17 +17,15 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/plans" element={<Plans />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/deposit" element={<Deposit />} />
         <Route path="/withdraw" element={<Withdraw />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/wallet" element={<WalletBox />} />
+        <Route path="/plans" element={<Plans />} />
         <Route path="/referrals" element={<Referrals />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/admin-payments" element={<AdminPayments />} />
+        <Route path="/admin/payments" element={<AdminPayments />} />
       </Routes>
     </Router>
   );
