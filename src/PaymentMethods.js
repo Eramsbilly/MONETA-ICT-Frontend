efe73d9d3667fa6f5064b0329dfe8200e6636284
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-27 13:55:23 +0100

    Create PaymentMethods.js

---
+++ src/PaymentMethods.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function PaymentMethods() {
+  return (
+    <div className="card">
+      <h4>Payment Methods</h4>
+      <ul>
+        <li>USDT (TRC20)</li>
+        <li>Bitcoin</li>
+        <li>MercadoPago (MX)</li>
+        <li>Pix (BR)</li>
+        <li>Nequi (CO)</li>
+        <li>Yape (PE)</li>
+      </ul>
+    </div>
+  );
+}
