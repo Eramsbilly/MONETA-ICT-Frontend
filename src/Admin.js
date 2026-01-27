d142ee6014ccacf8e269f6d3b8516474c644d37f
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-27 13:44:14 +0100

    Create Admin.js

---
+++ src/Admin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Admin() {
+  return (
+    <div className="card">
+      <h2>Admin Dashboard</h2>
+      <p>Users: 0</p>
+      <p>Deposits Pending: 0</p>
+    </div>
+  );
+}
