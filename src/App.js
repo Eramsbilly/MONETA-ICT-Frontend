57ee3d0fb05e6ecc624d1c9b89fdfdea0669b000
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-27 18:35:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./Navbar";
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
+import Plans from "./Plans";
 import Deposit from "./Deposit";
 import Withdraw from "./Withdraw";
-import Plans from "./Plans";
 import Referrals from "./Referrals";
 import Admin from "./Admin";
-import AdminPayments from "./AdminPayments";
+import Navbar from "./Navbar";
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Navbar />
@@ -20,15 +18,12 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/plans" element={<Plans />} />
         <Route path="/deposit" element={<Deposit />} />
         <Route path="/withdraw" element={<Withdraw />} />
-        <Route path="/plans" element={<Plans />} />
         <Route path="/referrals" element={<Referrals />} />
         <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/payments" element={<AdminPayments />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
