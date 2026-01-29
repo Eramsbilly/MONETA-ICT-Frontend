e3726fb8c72955f8d116a57d42052b2b3c0439c6
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-29 07:01:40 -0500

    Create Deposit.js

---
+++ src/Deposit.js
@@ -0,0 +1,12 @@
+export default function Deposit() {
+  return (
+    <div>
+      <h2>➕ Depósito</h2>
+      <p>Bancolombia Ahorro</p>
+      <p><b>00100007120</b></p>
+      <p>Jose Jimenez</p>
+      <input placeholder="Monto en COP" />
+      <button>Enviar comprobante</button>
+    </div>
+  );
+}
