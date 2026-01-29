25e81d7343b85b1112d8a2658b45a90aff9c7800
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-29 07:05:03 -0500

    Create Referrals.js

---
+++ src/Referrals.js
@@ -0,0 +1,9 @@
+export default function Referrals() {
+  return (
+    <div>
+      <h2>👥 Referidos</h2>
+      <p>Tu enlace:</p>
+      <code>https://moneta-ict.com/ref/ABC123</code>
+    </div>
+  );
+}
