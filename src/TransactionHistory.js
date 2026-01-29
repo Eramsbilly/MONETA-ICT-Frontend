20ccde5ef788e8d13dce946698d9d090dca6f742
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-29 07:03:54 -0500

    Create TransactionHistory.js

---
+++ src/TransactionHistory.js
@@ -0,0 +1,11 @@
+export default function TransactionHistory() {
+  return (
+    <div>
+      <h2>📜 Historial</h2>
+      <ul>
+        <li>Depósito – $50.000 COP – Pendiente</li>
+        <li>Retiro – $25.000 COP – Aprobado</li>
+      </ul>
+    </div>
+  );
+}
