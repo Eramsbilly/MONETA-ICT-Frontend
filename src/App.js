f0be5d97cdd06241e3169c747bb0df676e52123f
Eramsbilly <eramsbillionz8@gmail.com>
2026-01-29 08:05:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
-
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
 import Plans from "./Plans";
-import Wallet from "./Wallet";
 import Deposit from "./Deposit";
 import Withdraw from "./Withdraw";
-import TransactionHistory from "./TransactionHistory";
-import Profile from "./Profile";
+import Transactions from "./Transactions";
 import Referrals from "./Referrals";
 import Admin from "./Admin";
 
@@ -22,14 +19,12 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/plans" element={<Plans />} />
-        <Route path="/wallet" element={<Wallet />} />
         <Route path="/deposit" element={<Deposit />} />
         <Route path="/withdraw" element={<Withdraw />} />
-        <Route path="/history" element={<TransactionHistory />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/transactions" element={<Transactions />} />
         <Route path="/referrals" element={<Referrals />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
     </BrowserRouter>
   );
-  }
+}
